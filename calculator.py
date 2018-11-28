09bf5ddefb6a14d82427d1f908be0d2e70f955c4
dolapoadebara <45333202+dolapoadebara@users.noreply.github.com>
2018-11-28 11:04:51 +0100

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,26 @@
+#grade calculator
+
+
+x=int(input("please enter a score:"))
+
+if x>=70 and x<=100:
+    print("A")
+elif x>=60 and x<=69:
+    print("B")
+elif x>=50 and x<=59:
+    print("C")
+elif x>=45 and x<=49:
+    print("D")
+elif x>=40 and x<=44:
+    print("E")
+elif x>=0 and x<=39:
+    print("F")
+else:
+    print('please input any score within 0 and 100')
+
+
+
+
+
+
+    
